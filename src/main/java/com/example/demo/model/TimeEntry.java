e9bd175e964b06f156a70d445d57ccd0af7cc13a
Radojko Drcelic <rdrcelic@pivotal.io>
2018-06-06 13:48:28 +0200

    enabled sending json payload to createEntry

--- src/main/java/com/example/demo/model/TimeEntry.java
+++ src/main/java/com/example/demo/model/TimeEntry.java
@@ -1,5 +1,9 @@
 package com.example.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,14 +13,20 @@ import java.time.LocalDate;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TimeEntry {
     private long id;
     private long projectId;
     private long userId;
+    @JsonFormat(shape= JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate date;
     private int hours;
 
-    public TimeEntry(long projectId, long userId, LocalDate date, int hours) {
+    @JsonCreator
+    public TimeEntry(@JsonProperty(value = "projectId") long projectId,
+                     @JsonProperty(value = "userId") long userId,
+                     @JsonProperty(value = "date") LocalDate date,
+                     @JsonProperty(value = "hours") int hours) {
         this.projectId = projectId;
         this.userId = userId;
         this.date = date;
