514c4c8b81415f6eee325c4fd8e4b079a9056fca
Radojko Drcelic <rdrcelic@pivotal.io>
2018-07-11 09:44:03 +0200

    added artifact versioning

--- ci/scripts/maven-build.sh
+++ ci/scripts/maven-build.sh
@@ -3,8 +3,16 @@ set -e
 
 ./source-code/ci/scripts/generate-mvn-settings.sh
 
+./pipeline/tasks/common.sh
+
+VERSION=$(build_version "./version" "number" "./source-code" $BRANCH)
+echo "Version to build: ${VERSION}"
+
 cd source-code || echo "missing input resource: source-code"
 
 echo "Using MAVEN_OPTS: ${MAVEN_OPTS}"
 
 mvn verify ${MAVEN_ARGS}
+
+echo "Copying artifact to ./build "
+cp target/*.jar ../build
