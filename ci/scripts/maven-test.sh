7f78e30623d1b7403cafa50a94a8b978651a4694
Radojko Drcelic <rdrcelic@pivotal.io>
2018-07-11 18:22:41 +0200

    moved sensitive app user credentials to secrets

--- ci/scripts/maven-test.sh
+++ ci/scripts/maven-test.sh
@@ -9,4 +9,4 @@ cd acceptance-test || echo "missing input resource: acceptance-test"
 APP_URI=https://${APP_HOST}.${APP_DOMAIN}
 
 echo "Running acceptance tests against ${APP_URI}"
-mvn test -DbaseURI=${APP_URI} 
+mvn test -DbaseURI=${APP_URI} -DappUser=${APP_USER} -DappUserPassword=${APP_USERPWD}}
