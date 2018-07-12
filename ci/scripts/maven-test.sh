cc9f51d5027a339a37af66505a464c3ce124f5d5
Radojko Drcelic <rdrcelic@pivotal.io>
2018-07-12 10:21:49 +0200

    try to fix basic auth for actuator endpoint calls

--- ci/scripts/maven-test.sh
+++ ci/scripts/maven-test.sh
@@ -7,6 +7,9 @@ set -e
 cd acceptance-test || echo "missing input resource: acceptance-test"
 
 APP_URI=https://${APP_HOST}.${APP_DOMAIN}
+USER=${APP_USER}
+echo "with user: ${USER}"
+PASSWORD=${APP_USERPWD}
 
 echo "Running acceptance tests against ${APP_URI}"
-mvn test -DbaseURI=${APP_URI} -DappUser=${APP_USER} -DappUserPassword=${APP_USERPWD}}
+mvn test -DbaseURI=${APP_URI} -DappUser=${USER} -DappUserPassword=${PASSWORD}
